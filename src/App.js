e8e22f6c6a08097ed890575b5c4411ab14abdc68
Serg K <serega.vegas@gmail.com>
2018-12-22 15:29:38 +0200

    Add display

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Display from "./components/Display";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      current: null,
+      operation: null
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Display value={this.state.current || this.state.total || "0"} />
       </div>
     );
   }
